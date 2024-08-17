280827a69907a5ccc6f496352cd98222177c0a1a
MisaghEmir <misagh.amir@yahoo.com>
2024-08-17 16:22:25 -0700

    Start Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className=" text-black text-5xl p-24">
+     The World’s
+Best Creators
+Are On Behance
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      AcuminPro : 'Acumin Pro',
+      AcuminProItalic : 'Acumin Pro Italic',
+      AcuminProBold : 'Acumin Pro Bold',
+      AcuminProBoldItalic : 'Acumin Pro Bold Italic',
+    }
+  },
+  plugins: [],
+}
+
